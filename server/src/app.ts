ea7f49432f2b463d3eb69a4dda97fb8e0dcd32ec
ibritvic <ivbritvic@gmail.com>
2018-10-16 15:04:21 +0200

    Vue frontend

---
+++ server/src/app.ts
@@ -0,0 +1,94 @@
+import * as express from 'express';
+import * as path from 'path';
+import * as cookieParser from 'cookie-parser';
+import * as logger from 'morgan';
+import { Request, Response, NextFunction } from 'express';
+import { indexRoutes } from './routes';
+import { userRoutes } from './routes/users';
+import session from 'express-session';
+import mongo from 'connect-mongo';
+import * as bluebird from 'bluebird';
+import passport from 'passport';
+// API keys and Passport configuration
+import * as passportConfig from './config/passport';
+import {SESSION_SECRET, MONGODB_URI} from './config/settings';
+import * as mongoose from 'mongoose';
+
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.database();
+    this.routes();
+    this.catchErrors();
+  }
+
+  /**
+   * Middlewares config
+   */
+  private middlewares(): void {
+
+    this.app.use(logger('dev'));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cookieParser());
+  }
+
+
+  /**
+   * Config mongo database
+   */
+
+  private database(): void {
+    const MongoStore = mongo(session);
+    // Connect to MongoDB
+    const mongoUrl = MONGODB_URI;
+    (<any>mongoose).Promise = bluebird;
+    mongoose.connect(mongoUrl, {useMongoClient: true}).then(
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+    ).catch(err => {
+      console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+      // process.exit();
+    });
+    this.app.use(session({
+        resave: true,
+        saveUninitialized: true,
+        secret: SESSION_SECRET,
+        store: new MongoStore({
+          url: mongoUrl,
+          autoReconnect: true,
+      }),
+    }));
+  }
+
+  /**
+   * Error Handlers
+   */
+  private catchErrors(): void {
+    this.app.use((req: Request, res: Response, next: NextFunction) => {
+      const err: any = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
+
+    this.app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+      const statusCode = err.status || 500;
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+      res.status(statusCode).send('Server Error');
+    });
+  }
+  /**
+   * Api Routes
+   */
+  private routes(): void {
+    this.app.use('/', indexRoutes);
+    this.app.use('/user', userRoutes);
+  }
+
+
+}
+export default new App().app;
---
+++ server/src/stellar/index.ts
@@ -0,0 +1,78 @@
+// Classes required to create new account
+import {
+    Keypair, // Keypair represents public and secret keys.
+    Network, // Network provides helper methods to get the passphrase or id for different stellar networks.
+    Operation, // Operation helps you represent/build operations in Stellar network.
+    Server, // Server handles the network connections.
+    TransactionBuilder // Helps you construct transactions.
+  } from 'stellar-sdk';
+import * as fetch from 'node-fetch'
+
+/**
+ *  Generates a keypair and funds account with friendbot
+ */
+async function createTestAccount() {
+  const pair = Keypair.random()
+  console.log('Requesting Lumens')
+
+  await fetch(`https://horizon-testnet.stellar.org/friendbot?addr=${pair.publicKey()}`)
+
+  return pair;
+}
+
+async function run() {
+  const pair = await createTestAccount()
+
+  console.log(`
+    Congrats, you have a Stellar account in the test network!
+    seed: ${pair.secret()}
+    id: ${pair.publicKey()}
+  `)
+
+  const url = `https://horizon-testnet.stellar.org/accounts/${pair.publicKey()}`
+
+  console.log(`
+    Loading account from test network:
+    ${url}
+  `)
+
+  const response = await fetch(url)
+  const payload = await response.json()
+}
+  /*
+export async function pay(){
+
+  try {
+      // Tell the Stellar SDK you are using the testnet
+      Network.useTestNetwork();
+      // point to testnet host
+      const stellarServer = new Server('https://horizon-testnet.stellar.org');
+    
+      // Never put values like the an account seed in code.
+      //const provisionerKeyPair = Keypair.fromSecret('SA72TGXRHE26WC5G5MTNURFUFBHZHTIQKF5AQWRXJMJGZUF4XY6HFWJ4')
+    
+      // Load account from Stellar
+      const provisioner = await stellarServer.loadAccount(provisionerKeyPair.publicKey())
+    
+      console.log('creating account in ledger', keypair.publicKey())
+      const transaction = new TransactionBuilder(provisioner)
+            .addOperation(
+              // Operation to create new accounts
+              Operation.createAccount({
+                destination: keypair.publicKey(),
+                startingBalance: '2'
+              })
+            ).build()
+    
+      // Sign the transaction above
+      transaction.sign(provisionerKeyPair)
+    
+      // Submit transaction to the server
+      const result = await stellarServer.submitTransaction(transaction);
+      console.log('Account created: ', result)
+    } catch (e) {
+      console.log('Stellar account not created.', e)
+    }
+};
+*/
+  
